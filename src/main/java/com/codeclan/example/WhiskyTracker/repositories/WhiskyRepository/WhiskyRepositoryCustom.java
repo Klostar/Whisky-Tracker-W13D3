64e4b64afcc824e5522840566eaa46cc8c0c9b56
chloe alcorn <ca.alcorn@outlook.com>
2018-10-24 15:15:59 +0100

    can get whisky of a particular year

--- src/main/java/com/codeclan/example/WhiskyTracker/repositories/WhiskyRepository/WhiskyRepositoryCustom.java
+++ src/main/java/com/codeclan/example/WhiskyTracker/repositories/WhiskyRepository/WhiskyRepositoryCustom.java
@@ -1,4 +1,11 @@
 package com.codeclan.example.WhiskyTracker.repositories.WhiskyRepository;
 
+import com.codeclan.example.WhiskyTracker.models.Whisky;
+
+import java.util.List;
+
 public interface WhiskyRepositoryCustom {
+    //TODO: put all the custom querys in here!
+
+    List<Whisky>getAllWhiskyFromParticularYear(int year);
 }
