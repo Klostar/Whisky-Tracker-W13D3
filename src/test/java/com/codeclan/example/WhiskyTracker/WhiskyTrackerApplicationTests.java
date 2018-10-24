1cd1d8d04e6d99f22c6bdc7ae40c1f2b5447ef94
chloe alcorn <ca.alcorn@outlook.com>
2018-10-24 16:08:02 +0100

    can get distillery by region

--- src/test/java/com/codeclan/example/WhiskyTracker/WhiskyTrackerApplicationTests.java
+++ src/test/java/com/codeclan/example/WhiskyTracker/WhiskyTrackerApplicationTests.java
@@ -1,5 +1,6 @@
 package com.codeclan.example.WhiskyTracker;
 
+import com.codeclan.example.WhiskyTracker.models.Distillery;
 import com.codeclan.example.WhiskyTracker.models.Whisky;
 import com.codeclan.example.WhiskyTracker.repositories.DistilleryRepository.DistilleryRepository;
 import com.codeclan.example.WhiskyTracker.repositories.WhiskyRepository.WhiskyRepository;
@@ -36,4 +37,9 @@ public class WhiskyTrackerApplicationTests {
 		List<Whisky> results = whiskyRepository.getAllWhiskyFromRegion("Highland");
 	}
 
+
+	@Test
+	public void  getAllDistilleryFromRegion(){
+		List<Distillery> results = distilleryRepository.getAllDistilleryFromRegion("Highland");
+	}
 }
